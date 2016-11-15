55d6ce93c32d4ac9cb47de323814dd1f3c86a02f
Diaztanto <diaztanto.h@gmail.com>
2016-11-15 15:54:15 +0700

    Tambahan di buildClassifier biar isinya langsung persentase kemunculan, ga cuma jumlah kemunculan. Ini biar distributionForInstance ga ribet.

--- NBTubes.java
+++ NBTubes.java
@@ -20,17 +20,22 @@ import weka.core.Capabilities;
 public class NBTubes implements Classifier {
     
     public ArrayList<ArrayList<ArrayList<Integer>>> dataClassifier;
+    public ArrayList<ArrayList<ArrayList<Double>>> infoClassifier;
     //Urutan: 1. Atribut, 2. Domain
     //Kelas dan domain beserta jumlah instance nya dijumlah dari setiap data
     //domain dari sebuah atribut
     
     public NBTubes() {
-        dataClassifier = null;
+        dataClassifier = new ArrayList<>();
+        infoClassifier = new ArrayList<>();
     }
     
     @Override
     public void buildClassifier(Instances data) {
         int i, j, k, l;
+        //int sumAttr[] = {0, 0, 0, 0};
+        int sumVal = 0;
+        int[] sumClass = {0, 0, 0};
         
         int numAttr = data.get(0).numAttributes();
         //data.get(0).attribute(i).numValues() ini jumlah tipe nilai tiap atribut ke i
@@ -43,16 +48,19 @@ public class NBTubes implements Classifier {
         while (i < (numAttr-1)){
             //add new attribute, blm ada domainnya
             dataClassifier.add(new ArrayList<>());
+            infoClassifier.add(new ArrayList<>());
             
             j = 0;
             while (j < data.get(0).attribute(i).numValues()) {
                 //add new domain pada suatu atribut, blm ada perbedaan kelas
                 dataClassifier.get(i).add(new ArrayList<>());
+                infoClassifier.get(i).add(new ArrayList<>());
                 
                 k = 0;
                 while (k < data.get(0).attribute(numAttr-1).numValues()) {
                     //add new kelas di dalam atribut, domain spesifik                    
                     dataClassifier.get(i).get(j).add(0);
+                    infoClassifier.get(i).get(j).add(0.0);
                     k++;
                 }
                 
@@ -65,37 +73,47 @@ public class NBTubes implements Classifier {
         //reading from instances
         i = 0;
         while (i < data.size()) {
-            
-            
             j = 0;
             while (j < numAttr-1) {
-                
                 dataClassifier.get(j).get((int) data.get(i).value(j)).
-                        set((int) data.get(i).value(numAttr-1), dataClassifier.get(j).get((int) data.get(i).value(j)).get((int) data.get(i).value(numAttr-1))+1);
+                        set((int) data.get(i).value(numAttr-1), 
+                                dataClassifier.get(j).get(
+                                        (int) data.get(i).value(j)).get(
+                                                (int) data.get(i).value(numAttr-1))+1);
                 
-                /*
+                if (j == 0) {
+                    sumClass[(int) data.get(i).value(numAttr-1)]++;
+                }
+                
+                j++;
+            }
+            
+            i++;
+        }
+        
+        //getting double values, jumlahNilaiXdiAtrY/jumlahAtrYDiKelasZ
+        
+        i = 0;
+        while (i < dataClassifier.size())
+        {
+            j = 0;
+            while (j < dataClassifier.get(i).size()) {
                 k = 0;
-                while (k < data.get(0).attribute(j).numValues()) {
-                    
-                    l = 0;                    
-                    while (l < numClasses) {   
-                        System.out.println("Sum pada atribut " + j + ", nilai " + k + ", kelas " + l + ": " + dataClassifier.get(j).get(k).get(l));
-                        
-                        if (data.get(i).value(numAttr-1) == l) {
-                            dataClassifier.get(j).get(k).set(l, (dataClassifier.get(j).get(k).get(l) + 1));
-                        }
-                        
-                        l++;
-                    }
-                    
+                while (k < dataClassifier.get(i).get(j).size()) {
+                    //dapetin sumVal(j) utk kelas(k)
+                    sumVal = dataClassifier.get(i).get(j).get(k);                    
+
+                    infoClassifier.get(i).
+                            get(j).
+                            set(k, (double)sumVal/sumClass[k]);
                     k++;
                 }
-                */
                 
                 j++;
             }
             i++;
         }
+        
     }
     
     @Override
@@ -103,7 +121,9 @@ public class NBTubes implements Classifier {
         //Fungsi ini menentukan probabilitas setiap kelas instance untuk instance 
         //yang ada di parameter fungsi
         
-        double[] a = new double[dataClassifier.size()];
+        double[] a = new double[dataClassifier.get(0).get(0).size()];
+        
+        //ambil dari infoClassifier
         
         return a;
     }
